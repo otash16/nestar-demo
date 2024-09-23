cb7f7484e0fe3106baf9c109c04327ecc6a89634
otash16 <otajonqutbiddinov1@gmail.com>
2024-09-23 16:52:15 +0500

    feat: organize and test routing system

---
+++ pages/community/index.tsx
@@ -0,0 +1,17 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
+import { useState } from "react";
+
+const Community: NextPage = () => {
+  const [title, setTitle] = useState<string>("hello");
+  return (
+    <div>
+      COMMUNITY{" "}
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        Press Me
+      </button>
+    </div>
+  );
+};
+
+export default withLayoutBasic(Community);
---
+++ pages/property/index.tsx
@@ -0,0 +1,13 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return (
+    <>
+      <Container>PROPERTY LIST</Container>
+    </>
+  );
+};
+
+export default withLayoutBasic(PropertyList);
